edfb06fae61b5357996f86f07c9e57fad9523afb
Diego Sanabria <diego.sanabria@neta.mx>
2022-03-25 16:15:11 -0500

    Initial Commit

---
+++ src/test/java/com/example/kusi/exer/ExerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kusi.exer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
