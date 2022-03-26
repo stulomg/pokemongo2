8709af2c62b430f33b580e909b1d0cf4b01c3915
stulomg-admin <stulomg@gmail.com>
2022-03-26 17:50:31 -0500

    Subiendo cambios en pom

--- src/main/java/com/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
+++ src/main/java/com/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
@@ -2,13 +2,18 @@ package com.springbootapp.springbootcallingexternalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class SpringBootCallingExternalApiApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringBootCallingExternalApiApplication.class, args);
+	}
+
+	@RequestMapping ("/hello")
+	public String hello() {
+		return "Hello World";
 	}
 
 }
