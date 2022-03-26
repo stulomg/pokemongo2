c316c2a0b9064342e6b16fa9e530f6197b0e632b
stulomg-admin <stulomg@gmail.com>
2022-03-26 17:03:23 -0500

    Agregando ahora si el SpringBoot

---
+++ src/main/java/com/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
@@ -0,0 +1,14 @@
+package com.springbootapp.springbootcallingexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCallingExternalApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBootCallingExternalApiApplication.class, args);
+	}
+
+}
