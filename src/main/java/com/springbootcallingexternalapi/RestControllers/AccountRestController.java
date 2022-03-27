2848d56ceb0fbf2ca057304d6ee5faa77cd1565b
stulomg-admin <stulomg@gmail.com>
2022-03-27 18:43:52 -0500

    New AccountModel and new retrieveAccountByOwner servise

--- src/main/java/com/springbootcallingexternalapi/RestControllers/AccountRestController.java
+++ src/main/java/com/springbootcallingexternalapi/RestControllers/AccountRestController.java
@@ -7,21 +7,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AccountRestController  {
+public class AccountRestController {
     @Autowired
     AccountService accountService;
 
     @DeleteMapping(value = "/account/delete/{owner}/{nombre}")
-    public ResponseEntity<Object> deleteAccount (@PathVariable String owner, @PathVariable String nombre){
-        try{
-        accountService.deleteAccount(owner, nombre);
-        return new ResponseEntity<>("Delete succesfully", HttpStatus.OK);
-        }catch (AccountOrOwnerNotFoundException e){
+    public ResponseEntity<Object> deleteAccount(@PathVariable String owner, @PathVariable String nombre) {
+        try {
+            accountService.deleteAccount(owner, nombre);
+            return new ResponseEntity<>("Delete succesfully", HttpStatus.OK);
+        } catch (AccountOrOwnerNotFoundException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
+    }
+
+    @GetMapping(value = "/account/find/{owner}")
+    public ResponseEntity<Object> retrieveAccountByOwner(@PathVariable String owner) {
+        return new ResponseEntity<>(  accountService.retrieveAccountByOwner(owner) , HttpStatus.OK);
     }
 }
