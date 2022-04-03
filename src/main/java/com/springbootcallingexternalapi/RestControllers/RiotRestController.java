e80c993924c6bc7f71936e0c6a87d0e29c1e643a
Darkkab <sasbenff@gmail.com>
2022-04-03 12:34:29 -0500

    update general

--- src/main/java/com/springbootcallingexternalapi/RestControllers/RiotRestController.java
+++ src/main/java/com/springbootcallingexternalapi/RestControllers/RiotRestController.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 public class RiotRestController {
