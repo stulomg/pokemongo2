ff19cc6fbf9702af45c2c1a4d9c29d3956bccf07
stulomg-admin <stulomg@gmail.com>
2022-04-08 23:59:45 -0500

    Controller test demo and new TestController Package

--- src/main/java/com/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
+++ src/main/java/com/springbootcallingexternalapi/SpringBootCallingExternalApiApplication.java
@@ -2,14 +2,18 @@ package com.springbootcallingexternalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import javax.sql.DataSource;
 
+import static org.springframework.context.annotation.FilterType.CUSTOM;
+
 @Configuration
 @SpringBootApplication()
+@ComponentScan(excludeFilters={@ComponentScan.Filter(type=CUSTOM,classes= TypeExcludeFilter.class),})
 public class SpringBootCallingExternalApiApplication {
 
 	public static void main(String[] args) {
@@ -19,5 +23,6 @@ public class SpringBootCallingExternalApiApplication {
 	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
 		return new JdbcTemplate(dataSource);
 	}
+
 
 }
