fce98cde54c50bb2d1678bcb8d3a783f2993acd3
stulomg-admin <stulomg@gmail.com>
2022-05-16 11:16:59 -0500

    last last update xdxdx

--- src/main/java/com/springbootcallingexternalapi/LeagueOfLegends/Mainsecurity.java
+++ src/main/java/com/springbootcallingexternalapi/LeagueOfLegends/Mainsecurity.java
@@ -56,6 +56,7 @@ public class Mainsecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
